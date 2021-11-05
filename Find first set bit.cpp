85b95e8943a288c6faf4f7d3dfc0b59a615907f8
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-11-05 18:52:56 +0530

    2 Problems

---
+++ Find first set bit.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        unsigned int position = 0;
+        while (n) {
+            ++position;
+            
+            if (n % 2 != 0)
+                break;
+            n /= 2;
+        }
+        
+        return position;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+  // } Driver Code Ends
