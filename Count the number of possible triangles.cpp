43c9e6558f70369b830a3f16d539f76bcf38e043
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-12-07 21:02:48 +0530

    2 Problems

---
+++ Count the number of possible triangles.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count the number of possible triangles.
+    int findNumberOfTriangles(int arr[], int n)
+    {
+        sort(arr, arr + n);
+        int result = 0;
+        for (int i = n - 1; i > 1; --i) {
+            int j = 0, k = i - 1;
+            while (j < k) {
+                if (arr[j] + arr[k] > arr[i]) {
+                    result += k - j;
+                    --k;
+                } else {
+                    ++j;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.findNumberOfTriangles(arr,n) <<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
