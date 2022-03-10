5e9c977bbf7fbabf0f16d68ec566771547710f8f
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-03-11 03:17:37 +0530

    1 Problem

---
+++ Height Using Parent Array.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findHeight(int N, int arr[]){
+        int last = N - 1, height = 1;
+        
+        while (arr[last] != -1) {
+            height++;
+            last = arr[last];
+        }
+        
+        return height;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.findHeight(N, arr)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
