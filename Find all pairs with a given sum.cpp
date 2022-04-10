bd80e5752c8ef06314e924fcad5847ec8f9f7ed3
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-04-10 20:29:42 +0530

    2 Problems

---
+++ Find all pairs with a given sum.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        unordered_set<int> elements;
+        for (int i = 0; i < M; ++i) {
+            elements.insert(B[i]);
+        }
+        
+        vector<pair<int, int>> result;
+        
+        sort(A, A + N);
+        for (int i = 0; i < N; ++i) {
+            if (elements.find(X - A[i]) != elements.end()) {
+                result.push_back(make_pair(A[i], X - A[i]));
+            }
+        }
+        return result;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}  // } Driver Code Ends
