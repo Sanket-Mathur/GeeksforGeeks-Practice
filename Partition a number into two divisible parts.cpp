10c65b523d97875318f31509c8d81dbe76426752
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-05-05 22:19:19 +0530

    1 Problem

---
+++ Partition a number into two divisible parts.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{   
+public:
+    string stringPartition(string S, int a, int b){
+        string x, y;
+        y = S;
+        
+        for (int i = 0; i < S.length() - 1; ++i) {
+            x += S[i];
+            y.erase(0, 1);
+            
+            if (stoi(x) % a == 0 && stoi(y) % b == 0) {
+                return x + " " + y;
+            }
+        }
+        
+        return "-1";
+    }
+};
+
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {   
+        int a,b;
+        string S;
+        cin >> S >> a >> b;
+        Solution ob;
+        cout << ob.stringPartition(S,a,b) << endl;
+    }
+    return 0; 
+}   // } Driver Code Ends
