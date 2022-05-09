02ce0aa342e35da579d79599af2952bfc78f38c6
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-05-09 19:20:20 +0530

    1 Problem

---
+++ Median in a row-wise sorted Matrix.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &matrix, int r, int c) {
+        int low = 0, high = 2000;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            int cnt = 0;
+            for (int i = 0; i < r; ++i) {
+                cnt += countLesser(matrix[i], mid);
+            }
+            
+            if (cnt <= r * c / 2) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
+    }
+    
+    int countLesser(vector<int> &arr, int value) {
+        int low = 0, high = arr.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] <= value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        cout<<obj.median(matrix, r, c)<<endl;        
+    }
+    return 0;
+}  // } Driver Code Ends
