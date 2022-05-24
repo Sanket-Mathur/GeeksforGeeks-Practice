818bc1f5739c904b415f3f39927d406336bbecd4
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-05-24 19:35:21 +0530

    1 Problem

---
+++ Remove Duplicates.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	string removeDups(string S) 
+	{
+	    string result;
+	    vector<bool> seen(256, 0);
+	    
+	    for (auto c : S) {
+	        if (!seen[(int)c]) {
+	            seen[(int)c] = true;
+	            result.push_back(c);
+	        }
+	    }
+	    
+	    return result;
+	}
+};
+
+// { Driver Code Starts.
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   		
+   		Solution ob;
+   		cout << ob.removeDups(s) << "\n";
+   		
+   	}
+
+    return 0;
+}  // } Driver Code Ends
