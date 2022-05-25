f072fd11954a40912ca55476cf32c69d46bc296d
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-05-25 19:18:42 +0530

    1 Problem

---
+++ Rightmost different bit.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        if(m == n) {
+            return -1;
+        }
+        
+        int k = 1;
+        
+        while(n % 2 == m % 2) {
+           k++;
+           n = n / 2;
+           m = m / 2;
+        }
+       
+        return k;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+}   // } Driver Code Ends
