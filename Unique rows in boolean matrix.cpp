53f37bd514534f3340c498ae0043048badd31583
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-06-03 22:01:51 +0530

    1 Problem

---
+++ Unique rows in boolean matrix.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col);
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+    	int n,m;
+    	cin>>n>>m;
+    	int a[MAX][MAX];
+    	for(int i=0;i<n;i++)
+    	for(int j=0;j<m;j++)
+    	cin>>a[i][j];
+    	vector<vector<int>> vec = uniqueRow(a,n,m);
+    	for(int i = 0;i<vec.size();i++){
+    	    for(int x : vec[i]){
+    	        cout<<x<<" ";
+    	    }
+    	    cout<<"$";
+    	}
+    	cout<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this function*/
+vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
+{
+    set<vector<int>> unique;
+    vector<vector<int>> result;
+    
+    for (int i = 0; i < row; ++i) {
+        vector<int> curr;
+        for (int j = 0; j < col; ++j) {
+            curr.push_back(M[i][j]);
+        }
+        
+        if (unique.find(curr) == unique.end()) {
+            unique.insert(curr);
+            result.push_back(curr);
+        }
+    }
+    
+    return result;
+}
