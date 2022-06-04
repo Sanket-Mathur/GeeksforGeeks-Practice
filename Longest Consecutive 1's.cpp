ab65942652226d2e1ecc0a7926e57e480fcae2fd
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-06-04 08:31:56 +0530

    1 Problem

---
+++ Longest Consecutive 1's.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int N)
+    {
+        int curr = 0, longest = 0;
+        
+        while (N) {
+            if (N & 1) {
+                ++curr;
+            } else {
+                longest = max(longest, curr);
+                curr = 0;
+            }
+            N >>= 1;
+        }
+        
+        return max(longest, curr);
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
