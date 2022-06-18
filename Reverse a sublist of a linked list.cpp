01389235bd1ff5cf138091f20644d4dda6f17b30
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-06-19 04:35:33 +0530

    1 Problem

---
+++ Reverse a sublist of a linked list.cpp
@@ -0,0 +1,117 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*Link list node 
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution
+{
+    public:
+    Node* reverseBetween(Node* head, int m, int n)
+    {
+        Node* prevPtr = m == 1 ? NULL : head;
+        for (int i = 2; i < m; ++i) {
+            prevPtr = prevPtr->next;
+        }
+        Node* forwPtr = prevPtr ? prevPtr->next : head;
+        
+        Node* prev = forwPtr;
+        Node* curr = prev->next;
+        for (int i = 0; i < n - m; ++i) {
+            Node* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        
+        if (prevPtr) {
+            prevPtr->next = prev;
+        } else {
+            head = prev;
+        }
+        forwPtr->next = curr;
+        
+        return head;
+    }
+};
+
+// { Driver Code Starts.
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int N, m, n;
+		cin >> N>>m>>n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		
+
+        Solution ob;
+
+		Node* newhead = ob.reverseBetween(head, m, n);
+		printList(newhead);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+  // } Driver Code Ends
