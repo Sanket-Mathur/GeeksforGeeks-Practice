8cf5f3444702ecd0effc8cfc2a355c609e881945
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-06-27 02:04:40 +0530

    1 Problem

---
+++ Change Bits.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> changeBits(int N) {
+        int bitnum = 0, temp = N;
+        while (temp > 0) {
+            temp >>= 1;
+            ++bitnum;
+        }
+        
+        int modified = (1 << bitnum) - 1;
+        
+        return {modified - N, modified};
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        Solution ob;
+        auto ans = ob.changeBits(N);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
