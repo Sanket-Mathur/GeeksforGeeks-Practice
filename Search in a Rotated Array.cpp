9cc052b2f324cfe794031e48a2564fc037d2761b
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-07-01 20:12:42 +0530

    1 Problem

---
+++ Search in a Rotated Array.cpp
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int search(int A[], int l, int h, int key){
+        int index = find(A, A + h + 1, key) - A;
+        return index == h + 1 ? -1 : index;
+    }
+};
+
+// { Driver Code Starts. 
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int A[n];
+        for(int i = 0; i < n; i++)
+            cin >> A[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(A, 0, n - 1, key) << endl;
+    }
+return 0;
+}  // } Driver Code Ends
