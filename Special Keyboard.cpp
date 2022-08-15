29b4674168512583b49bbe39832a0495633b7bed
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-08-15 15:33:28 +0530

    1 Problem

---
+++ Special Keyboard.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    long long int optimalKeys(int N){
+        if (N <= 6) {
+            return N;
+        }
+        
+        vector<int> dp(N + 1, 0);
+        for (int i = 1; i < 7; ++i) {
+            dp[i] = i;
+        }
+        for (int i = 7; i <= N; ++i) {
+            for (int j = i - 3; j > 0; --j) {
+                int curr = dp[j] * (i - j - 1);
+                if (curr > dp[i]) {
+                    dp[i] = curr;
+                }
+            }
+        }
+        
+        return dp[N];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.optimalKeys(N)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
