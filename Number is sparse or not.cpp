4b0ae2d8783b1a00dd3b0e0445652023353e1c32
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-08-25 21:23:42 +0530

    1 Problem

---
+++ Number is sparse or not.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+// C++ program to check if n is sparse or not
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to check if the number is sparse or not.
+    bool isSparse(int n)
+    {
+        bool last1 = false;
+        while (n > 0) {
+            if (n & 1) {
+                if (last1) {
+                    return false;
+                }
+                last1 = true;
+            } else {
+                last1 = false;
+            }
+            n >>= 1;
+        }
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+    int t,n;
+    
+    //testcases
+    cin>>t;
+    while(t--)
+    {
+        //initializing n
+        cin>>n;
+        
+        //printing 1 if isSparse() returns true
+        //else 0
+        Solution ob;
+        if(ob.isSparse(n)){
+            cout << "1" << endl;
+        }
+        else cout << "0" << endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
