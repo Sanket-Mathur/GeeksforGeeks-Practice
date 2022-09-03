f1629ed005bfbcbc5af34d3d86c05a2c84da0a84
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-09-03 16:23:22 +0530

    1 Problem

---
+++ Arithmetic Number.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        if (C == 0) {
+            return A == B;
+        }
+        
+        double n = (double)(B - A) / C;
+        if (n == floor(n) && n >= 0) {
+            return true;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A, B, C;
+        cin>>A>>B>>C;
+        
+        Solution ob;
+        cout<<ob.inSequence(A, B, C)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
