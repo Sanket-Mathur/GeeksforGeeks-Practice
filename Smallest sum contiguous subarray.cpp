74996fb01e8bc3794d062c7e4967d4e35597f9f9
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-09-05 21:15:19 +0530

    1 Problem

---
+++ Smallest sum contiguous subarray.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+      int minSum = INT_MAX, currSum = 0;
+      for (auto i : a) {
+          currSum += i;
+          minSum = min(minSum, currSum);
+          
+          if (currSum > 0) {
+              currSum = 0;
+          }
+      }
+      
+      return minSum;
+  }
+};
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        Solution ob;
+        
+        int ans = ob.smallestSumSubarray(a);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
