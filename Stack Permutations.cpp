84de83c592351a0a667062d98f50d890efa714fb
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-09-07 20:40:47 +0530

    1 Problem

---
+++ Stack Permutations.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int isStackPermutation(int N,vector<int> &A,vector<int> &B){
+        stack<int> stk;
+        int j = 0;
+        for (int i = 0; i < N; ++i) {
+            stk.push(A[i]);
+            
+            while (!stk.empty() && stk.top() == B[j]) {
+                stk.pop();
+                ++j;
+            }
+        }
+        
+        if (j == N) {
+            return 1;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        Solution ob;
+        cout<<ob.isStackPermutation(n,a,b)<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
