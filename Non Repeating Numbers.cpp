5a7f730e929b0ac7410e11ba493f494952e75640
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-11-09 10:47:27 +0530

    1 Problem

---
+++ Non Repeating Numbers.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        unordered_set<int> st;
+        for (auto i : nums) {
+            if (st.find(i) != st.end()) {
+                st.erase(i);
+            } else {
+                st.insert(i);
+            }
+        }
+        
+        vector<int> result(st.begin(), st.end());
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
