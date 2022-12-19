da3d988282020b8ab775dc8e62939998709057b7
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-12-19 12:04:07 +0530

    1 Problem

---
+++ Reverse Bits.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long X) {
+        string str = bitset<32>(X).to_string();
+        reverse(str.begin(), str.end());
+        
+        return bitset<32>(str).to_ulong();
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
