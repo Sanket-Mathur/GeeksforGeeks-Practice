4d2f80be1473efbea2aac594a69ed096d578ec94
Sanket-Mathur <rajeev.sanket@gmail.com>
2022-12-31 00:20:06 +0530

    1 Problem

---
+++ Remove duplicates in small prime array.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> removeDuplicate(vector<int>& arr, int n)
+    {
+        unordered_set<int> visited;
+        vector<int> result;
+        for (auto i : arr) {
+            if (visited.find(i) == visited.end()) {
+                result.push_back(i);
+                visited.insert(i);
+            }
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        vector<int> A(N);
+        for(int i=0;i<N;i++)
+        {
+            cin>>A[i];
+        }
+        Solution obj;
+        vector<int>result = obj.removeDuplicate(A,N);
+        for(int i =0;i<result.size();i++)
+            cout<<result[i]<<" ";
+        cout<<endl;
+
+    }
+}
+
+// } Driver Code Ends
