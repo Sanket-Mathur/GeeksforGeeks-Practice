d3d811d455840fca28c9302f82e84a241d7c2b1a
Sanket-Mathur <rajeev.sanket@gmail.com>
2023-03-01 09:04:40 +0530

    1 Problem

---
+++ Reverse both parts.cpp
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+struct Node *inputList()
+{
+    int n; // length of link list
+    scanf("%d ", &n);
+
+    int data;
+    cin >> data;
+    struct Node *head = new Node(data);
+    struct Node *tail = head;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        cin >> data;
+        tail->next = new Node(data);
+        tail = tail->next;
+    }
+    return head;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+Definition for singly Link List Node
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+
+class Solution
+{
+public:
+   
+    Node *reverse(Node *head, int k)
+    {
+        Node *prev = NULL;
+        Node *next = head;
+        for (int i = 0; i < k; ++i) {
+            Node *temp = next->next;
+            next->next = prev;
+            
+            prev = next;
+            next = temp;
+        }
+        
+        Node* newHead = prev;
+        
+        prev = NULL;
+        while (next) {
+            Node *temp = next->next;
+            next->next = prev;
+            
+            prev = next;
+            next = temp;
+        }
+        
+        head->next = prev;
+        
+        return newHead;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        struct Node *head = inputList();
+        int k;
+        cin >> k;
+
+        Solution obj;
+        Node *res = obj.reverse(head, k);
+
+        printList(res);
+    }
+}
+// } Driver Code Ends
